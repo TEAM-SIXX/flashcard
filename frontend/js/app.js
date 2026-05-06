0f05eefe190d523076f1baa0950fedffd297ede9
bamser3 <wunpun3@gmail.com>
2026-05-06 17:48:54 +0300

    Initial app.js setup

---
+++ frontend/js/app.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function App() {
+    return (
+        <div>
+            <header>
+                <h1>Project Description Flashcard App</h1>
+            </header>
+        </div>
+    )
+}
