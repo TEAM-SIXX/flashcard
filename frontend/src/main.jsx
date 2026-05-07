fbe3540e505f747c52551ae0142e0b4b63eb230c
bamser <111535201+bamser3@users.noreply.github.com>
2026-05-07 23:34:07 +0300

    Added routes with BrowserRouter in main.jsx (#9)

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './css/index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import App from "./App.jsx";
+
+import JDInput from "./components/jdInput";
+import TechDisplay from "./components/techDisplay";
+import FlashcardDeck from "./components/flashcardDeck";
+import StudyStats from "./components/studyStats";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<JDInput />} />
+      <Route path="/tech" element={<TechDisplay />} />
+      <Route path="/study" element={<FlashcardDeck />} />
+      <Route path="/dashboard" element={<StudyStats />} />
+    </Routes>
+  </BrowserRouter>
+);
